05f26b839109e91a9430bb5ca5ce0f4325949800
Martin-Louis Bright <mlbright@gmail.com>
2022-11-11 13:41:38 -0500

    feat: construct inclusion and exclusion regex

--- README.md
+++ README.md
@@ -1,27 +1,45 @@
 # basic-wordle-cheater
 
-Use regular expressions to cheat at [Wordle][wordle].
+A really corny way to cheat at Wordle.
 
 ## Usage
 
-Defaults to consulting the `/usr/share/dict/words` dictionary on MacOS.
-
 ```bash
-wordle-assistant "[^kd]r.ng"
-areng
-bring
-orang
-prong
-wrang
-wring
-wrong
-wrung
-```
+# Guesses at this point are:
+# SNAKE -> A and E are yellow
+# ABOVE -> A and E are yellow (repeated myself uselessly)
+# BAGEL -> A and E are yellow (here we know A is the fourth letter, but we make the computer do more work)
+wordle-assistant --green ....l --yellow "[a][a][a][e][e]" --grey bgovsnk
+ceral
+decal
+demal
+ectal
+equal
+ethal
+fecal
+feral
+fetal
+hemal
+ideal
+medal
+metal
+pedal
+petal
+pheal
+queal
+tepal
+ureal
+wheal
 
-To specify another dictionary file,
+# Chose 'IDEAL' and discover that AL are green, DE are yellow, and I is grey
 
-```bash
-wordle-assistant "s[^plt]ung" --dictionary "$HOME/Desktop/dictionary"
+./target/release/wordle-assistant --green ...al --yellow "[a][da][ae][e][e]" --grey ibgovsnk
+decal
+demal
+medal
+pedal
+
+# Chose 'MEDAL': solved!
 ```
 
 ## Build
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,70 @@
 use clap::Parser;
+use lazy_static::lazy_static;
 use regex::Regex;
-use std::{io::Read, path::PathBuf};
+use std::include_str;
 
-/// Simple program to greet a person
+/// Simple program to cheat at Wordle
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Regex for the word
-    regex: String,
-
-    /// Dictionary
-    #[clap(
-        short,
-        long,
-        required = false,
-        parse(from_os_str),
-        default_value = "/usr/share/dict/words"
-    )]
-    dictionary: PathBuf,
+    #[clap(short, long, required = false, default_value = ".....")]
+    green: String,
+    #[clap(short, long, required = true, default_value = "")]
+    grey: String,
+    #[clap(short, long, required = true, default_value = "")]
+    yellow: String,
 }
 
-fn find_word<'a>(word_regex: &'a str, word_list: Vec<&'a str>) -> Option<Vec<&'a str>> {
-    let re = Regex::new(word_regex).unwrap();
+fn find_words<'a>(
+    word_list: Vec<&'a str>,
+    regex_str: &str,
+    yellow: &str,
+    green: &str,
+) -> Option<Vec<&'a str>> {
+    let exclusions = Regex::new(regex_str).unwrap();
+    let inclusions = Regex::new(green).unwrap();
     let mut results: Vec<&str> = vec![];
     for word in word_list {
-        if word.len() != 5 {
-            continue;
-        }
-        if re.is_match(&word.to_lowercase()) {
+        if exclusions.is_match(&word.to_lowercase())
+            && inclusions.is_match(&word.to_lowercase())
+            && yellow
+                .chars()
+                .filter(|c| c.is_alphanumeric() && !c.is_ascii_digit())
+                .all(|c| word.contains(c))
+        {
             results.push(word);
         }
     }
-    if !results.is_empty() {
-        return Some(results);
+    if results.is_empty() {
+        None
+    } else {
+        Some(results)
     }
-    None
+}
+
+fn construct_exclusion_regex(grey: &str, yellow: &str) -> String {
+    let re = yellow.to_owned();
+    lazy_static! {
+        static ref YELLOW_LETTER_REGEX: Regex = Regex::new(r"\[([a-z]*)\]").unwrap();
+    }
+    let s = YELLOW_LETTER_REGEX.replace_all(&re, |caps: &regex::Captures| {
+        format!("[^{}{}]", &caps[1], grey)
+    });
+    s.to_string()
 }
 
 fn main() {
     let args = Args::parse();
-    let word_regex = args.regex;
-    let mut contents = String::new();
-    let mut f = std::fs::File::open(args.dictionary).expect("Unable to open the dictionary file");
-    f.read_to_string(&mut contents)
-        .expect("Unable read the dictionary file");
-    let word_list: Vec<&str> = contents.split('\n').collect();
-    let result = find_word(&word_regex, word_list);
-    match result {
+    let five_letter_words = include_str!("five-letter-words");
+    let word_list = five_letter_words.split('\n').collect();
+    let regex_str: String = construct_exclusion_regex(&args.grey, &args.yellow);
+    let results = find_words(word_list, &regex_str, &args.yellow, &args.green);
+    match results {
         Some(words) => {
             for word in words {
                 println!("{}", word.to_lowercase());
             }
         }
-        None => println!("No result"),
+        None => println!("No results"),
     }
 }
