7dde345c3c2d4879f042d729c956dc81d2f5b96c
Martin-Louis Bright <mlbright@gmail.com>
2022-11-28 16:48:06 -0500

    fix: use yellow correctly

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,6 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use std::include_str;
 
-macro_rules! regex {
-    ($re:literal $(,)?) => {{
-        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
-        RE.get_or_init(|| regex::Regex::new($re).unwrap())
-    }};
-}
-
 /// Simple program to cheat at Wordle
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -36,20 +29,19 @@ fn greens(word: &str, green: &str) -> bool {
 
 fn find_words<'a>(
     word_list: Vec<&'a str>,
-    regex_str: &str,
     grey: &str,
     yellow: &str,
     green: &str,
 ) -> Option<Vec<&'a str>> {
-    let exclusions_filter = Regex::new(regex_str).unwrap();
+    let exclusions_filter = Regex::new(yellow).unwrap();
     let mut results: Vec<&str> = vec![];
     for word in word_list {
         if greens(word, green)
+            && !exclusions_filter.is_match(word)
             && yellow
                 .chars()
                 .filter(|c| c.is_alphanumeric() && !c.is_ascii_digit())
                 .all(|c| word.contains(c))
-            && exclusions_filter.is_match(word)
             && grey
                 .chars()
                 .filter(|c| c.is_alphanumeric() && !c.is_ascii_digit())
@@ -65,24 +57,13 @@ fn find_words<'a>(
     }
 }
 
-fn construct_exclusion_regex(grey: &str, yellow: &str) -> String {
-    let re_str = yellow.to_owned();
-    let re = regex!(r"\[([a-z]*)\]");
-    re.replace_all(&re_str, |caps: &regex::Captures| {
-        format!("[^{}{}]", &caps[1], grey)
-    })
-    .to_string()
-}
-
 static FIVE_LETTER_WORDS: Lazy<Vec<&str>> =
     Lazy::new(|| include_str!("five-letter-words").split('\n').collect());
 
 fn main() {
     let args = Args::parse();
-    let regex_str: String = construct_exclusion_regex(&args.grey, &args.yellow);
     match find_words(
         FIVE_LETTER_WORDS.to_vec(),
-        &regex_str,
         &args.grey,
         &args.yellow,
         &args.green,
